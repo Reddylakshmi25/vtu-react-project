d94feffc3d3b14fe2b3654d44e22c2e1b0354b85
= <=>
2024-02-08 15:14:43 +0530

    Added 3 divisions,created button

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import { Carousel,Row,Col,Button } from "react-bootstrap";
 const HomeScreen = () => {
 
     return(
@@ -52,6 +52,62 @@ const HomeScreen = () => {
                 />
             </Carousel.Item>
         </Carousel>
+        <br/><br/>
+        <div>
+            <Row>
+                <Col>
+                    <h3><a href="#">Vision</a></h3>
+                    <p className='p-style'>
+                      To become an outstanding Technological University at the cutting edge of Science and Technology
+                      that produces world class Knowledge-delivery, Research, 
+                      Extension and Leadership in Technology innovation 
+                      for Industry and Society.
+                    </p>
+
+                    <p className='p-style'>
+                    To become an outstanding Technological University at the cutting edge of Science and Technology
+                     that produces world class Knowledge-delivery, Research,
+                      Extension and Leadership in Technology innovation
+                       for Industry and Society.
+                    </p>
+
+                    <Button variant="warning">Read more</Button>
+                </Col>
+
+                <Col>
+                    <h3><a href="#">Mission</a></h3>
+                    <p className='p-style'>
+                    To plan the development of technical education,
+                    to establish value-based and need-based education and training in engineering and technology,
+                    with a view to generate qualified and competent manpower,
+                    responsive to technological and societal needs.
+                    </p>
+                    <p className='p-style'>
+                    To plan the development of technical education,
+                    to establish value-based and need-based education and training in engineering and technology,
+                    with a view to generate qualified and competent manpower,
+                    responsive to technological and societal needs.
+                    </p>
+
+                    <Button variant="warning">Read more</Button>
+                </Col>
+
+                <Col>
+                    <h3><a href="#">Mandate</a></h3>
+                    <p className='p-style'>
+                       The Visvesvaraya Technological University has been established by the Government of the Karnataka
+                    in order to Promote planned and sustainable development of technical education consistent with state and national policies.
+                    Design need based programs that ensure adequate human resource of the right kind.
+                    Establish objective evaluation and certification systems for students,programs and institutions.
+                    Collaborate with national and international institutions R&D establishments organizations like AICTE, MHRD, UGC, etc.
+                    </p>
+
+                    <Button variant="warning">Read more</Button>
+                </Col>
+
+            </Row>
+        </div>
+
         </div>
         </>
     )
